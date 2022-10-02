659195df61a37af61b244dc1731acdc69a99ded6
juandiegows <mejiamaestrejuandiego@gmail.com>
2022-10-02 18:22:26 -0500

    recatchap

---
+++ Recaptchat/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Recaptchat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p1
+            // 
+            this.p1.Location = new System.Drawing.Point(12, 12);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(125, 40);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.p1.TabIndex = 0;
+            this.p1.TabStop = false;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.Green;
+            this.btnGenerate.Location = new System.Drawing.Point(12, 67);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(125, 42);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(274, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirmar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.ForeColor = System.Drawing.Color.Green;
+            this.btnConfirmar.Location = new System.Drawing.Point(274, 68);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(125, 42);
+            this.btnConfirmar.TabIndex = 1;
+            this.btnConfirmar.Text = "Comprobar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 122);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.p1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConfirmar;
+    }
+}
+
---
+++ Recaptchat/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recaptchat
+{
+    public partial class Form1 : Form
+    {
+        String textCaptchat = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private String GetTextRandom()
+        {
+            MD5CryptoServiceProvider mD5Crypto = new MD5CryptoServiceProvider();
+            long numRandom = new Random().Next(0, 999999999);
+            var byteA = mD5Crypto.ComputeHash(System.Text.ASCIIEncoding.ASCII.GetBytes(numRandom.ToString()));
+            String text = "";
+            foreach (var item in byteA)
+            {
+                text += item.ToString("x2");
+            }
+            return text.Substring(6, 6);
+        }
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(p1.Width, p1.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Font font = new System.Drawing.Font("NSimSun", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            String text = GetTextRandom();
+            if(int.TryParse(text, out int numRandom))
+            {
+                text = GetTextRandom();
+            }
+            textCaptchat = text;
+            graphics.DrawString(text, font, Brushes.Red, new Point(0, 0));
+            //graphics.DrawLine(new Pen(Brushes.Blue,2), 0, (int)(p1.Height / 2), p1.Width,(int) (p1.Height / 2));
+            Random random = new Random();
+            
+            graphics.DrawRectangle(new Pen(Brushes.Blue, 2), random.Next(0,p1.Width-20), random.Next(0, p1.Height - 20), 20,20);
+            graphics.DrawRectangle(new Pen(Brushes.Yellow, 2), random.Next(0,p1.Width-20), random.Next(0, p1.Height - 20), 20,20);
+            graphics.DrawRectangle(new Pen(Brushes.OrangeRed, 2), random.Next(0,p1.Width-20), random.Next(0, p1.Height - 20), 20,20);
+            graphics.DrawRectangle(new Pen(Brushes.Green, 2), random.Next(0,p1.Width-20), random.Next(0, p1.Height - 20), 20,20);
+            graphics.ResetTransform();
+            p1.Image = bitmap;
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            if(textCaptchat == textBox1.Text)
+            {
+                MessageBox.Show("Captchat correct");
+            }else
+            {
+                MessageBox.Show("Captchat incorrect");
+            }
+        }
+    }
+}
